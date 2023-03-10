02361ab193b6bb161b7fffa71a6cf03845dd6dc0
Aaron Humphres <aaronhumphres@yahoo.com>
2023-03-10 12:52:11 -0700

    Posts successful - newest first

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Define the home view
 def home(request):
+  posts = Post.objects.all()
   # Include an .html file extension - unlike when rendering EJS templates
-  return render(request, 'home.html')
+  return render(request, 'home.html', {'posts' : posts})
 
 # about route
 def about(request):
