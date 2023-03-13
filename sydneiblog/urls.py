b1b034150962d6ef610667335f116304854d2f47
Aaron Humphres <aaronhumphres@yahoo.com>
2023-03-13 11:45:43 -0700

    update to navbar

--- sydneiblog/urls.py
+++ sydneiblog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # '' represents the "starts with" path
+  
     path('', include('main_app.urls')),
+    
 ]
