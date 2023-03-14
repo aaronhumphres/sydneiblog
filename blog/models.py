71c59f5ddade9b0bd7fd5d9d0e4c87d5fe32b246
Aaron Humphres <aaronhumphres@yahoo.com>
2023-03-14 16:49:50 -0700

    added categories

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
+
+    class Meta:
+        ordering = ('title',)
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
+    category = models.ForeignKey(Category, name='posts', on_delete=models.CASCADE)
     title = models.CharField(max_length = 255)
     slug = models.SlugField()
     intro = models.TextField()
